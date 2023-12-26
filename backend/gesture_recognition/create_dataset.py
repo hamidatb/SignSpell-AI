8c7fe1af7db3b919a1d4d3fd029abd4bb0e6c17c
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-12-26 12:30:46 -0700

    Coding

--- backend/gesture_recognition/create_dataset.py
+++ backend/gesture_recognition/create_dataset.py
@@ -3,6 +3,19 @@ import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
+# This is the handtracking model from media pipe
+mp_hands = mp.solutions.hands 
+
+# This is used to draw the hand landmarks on images for visualization.
+mp_drawings = mp.solutions.drawing_utils
+
+# This offers styling options for the landmarks drawn on the images, such as different colors or line thicknesses.
+mp_drawing_styles = mp.solutions.drawing_styles
+
+# Setting true means that Im running hand detection on every individual image.
+# Detections below 30% confidence will be ignored.
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
 # Getting the directory where this script file is located
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -25,6 +38,20 @@ for dir_ in os.listdir(DATA_DIR):
                 img = cv2.imread(img_full_path)
                 # Changes the image data into rgb so that matplotlib can actually comprehend whats happening
                 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                
+                # Iterate over all the landmarks I'v detected in this image
+                results = hands.process(img_rgb) 
+                # Have to make sure we are detecing at least one hand before continuing:
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        # For each result we are going to draw the landmarks on top of the image essentially.
+                        mp_drawings.draw_landmarks(
+                            img_rgb,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS, # The predefined connections between hand landmarks (i.e., how the landmarks are linked to each other).
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style()
+                        )
 
                 plt.figure()
                 plt.imshow(img_rgb) # Loading the image for each image in the path
