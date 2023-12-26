6f69b953a6550bb9a2428fee795d4d4fdd305fb8
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-12-26 12:04:32 -0700

    Deleting excess training files

--- backend/gesture_recognition/create_dataset.py
+++ backend/gesture_recognition/create_dataset.py
@@ -16,12 +16,19 @@ for dir_ in os.listdir(DATA_DIR):
     # Make sure it's a directory
     if os.path.isdir(class_dir):
         # Loop through each image in the subdirectory
-        for img_path in os.listdir(class_dir):
+        for img_path in os.listdir(class_dir)[:1]: # Using slicing to ensure I'm only showing 1
             # Construct the full path to the image
             img_full_path = os.path.join(class_dir, img_path)
             # Make sure it's a file before reading
             if os.path.isfile(img_full_path):
                 # Read the image using OpenCV
                 img = cv2.imread(img_full_path)
+                # Changes the image data into rgb so that matplotlib can actually comprehend whats happening
                 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+                plt.figure()
+                plt.imshow(img_rgb) # Loading the image for each image in the path
+
+# Using MatplotLib to show the images!
+plt.show()
+ 
