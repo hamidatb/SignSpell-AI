59cf42e5ee026a62ba17912f40d6ed8614c241da
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-12-31 16:19:43 -0700

    Updates.

--- backend/gesture_model/practice_mode.py
+++ backend/gesture_model/practice_mode.py
@@ -4,11 +4,11 @@ import os
 import pickle
 import numpy as np
 import time
-import openai
 import sys
+import random
 
 # Getting the directory of this file and the model file.
-def get_directory():
+def get_directory() -> str:
     # Getting the directory where this script file is located
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
     # Setting the directory where the data is stored, relative to this script file
@@ -20,90 +20,106 @@ def get_directory():
 def open_model(SCRIPT_DIR, DATA_DIR):
     model_dict = pickle.load(open(DATA_DIR, "rb"))
     model = model_dict['model']
-    return  model
+    return model
 
-# Loading the users progress from their practices.
-def load_progress(file_path):
-    """
-    Loads the user's progress from a file.
-    :param file_path: The path to the progress file.
-    :return: A dictionary containing the user's progress.
-    """
+# Loading the users progress from their practices. 
+def load_progress(file_path:str) -> dict:
     if os.path.exists(file_path):
         with open(file_path, 'rb') as file:
             return pickle.load(file)
     else:
         return {chr(65+i): {'attempts': 0, 'correct': 0} for i in range(26)}
 
-# Saving the users progress from their practices.
-def save_progress(progress, file_path):
+# Saving the users progress from their practices. 
+def save_progress(progress, file_path) -> None:
     """
-    Saving the user's progress in the practice mode to a file.
-    :param progress: The progress dictionary.
-    :param file_path: The path to the progress file.
+    Note: Will have to decide how the progress data will be written and stored.
     """
     with open(file_path, 'wb') as file:
         pickle.dump(progress, file)
 
-def practice(model, target_letter):
+# Function to select the next letter to practice
+def select_letter(progress):
+    """
+    Selects the next letter for practice using a simple heuristic:
+    Less accuracy and fewer attempts increase the likelihood of selection.
+    """
+    letters = list(progress.keys())
+    # Calculate weights inversely proportional to accuracy and attempts
+    weights = [1 / (progress[letter]['correct'] / (progress[letter]['attempts'] + 1)) for letter in letters]
+    return random.choices(letters, weights)[0]
+
+def initialize_camera():
     cap = cv2.VideoCapture(0)
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5)
-    labels_dict = {i: chr(65 + i) for i in range(26)}  # Mapping integers to alphabet letters
+    return cap, hands
 
-    start_time = time.time()
-    
+def capture_and_process_frame(cap, hands):
+    ret, frame = cap.read()
+    if not ret:
+        sys.exit("Failed to grab frame")
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(frame_rgb)
+    return frame, results
+
+def make_prediction(model, results, frame):
+    data_loc = []  # To store hand landmark data
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark:
+                x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                data_loc.extend([x, y])
+
+        # Replace this with your model's prediction logic
+        prediction = model.predict([np.asarray(data_loc)])
+        predicted_character = chr(65 + int(prediction[0]))  # Assuming your model predicts an integer
+        return predicted_character
+    return None
+
+def update_and_display(frame, target_letter, predicted_character):
+    is_correct = predicted_character.lower() == target_letter.lower()
+    # Display the prediction and target letter on the frame
+    cv2.putText(frame, predicted_character, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 3)
+    cv2.putText(frame, f"Show letter: {target_letter}", (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+    cv2.imshow("Practice Mode", frame)
+    return is_correct
+
+def practice_loop(model, progress, file_path):
+    cap, hands = initialize_camera()
+
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            sys.exit("Failed to grab frame")
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(frame_rgb)
+        target_letter = select_letter(progress)
+        print(f"Practice this letter: {target_letter}")
+        start_time = time.time()
 
-        data_loc = []
-        x_ = []
-        y_ = []
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i, landmark in enumerate(hand_landmarks.landmark):
-                    x, y = landmark.x, landmark.y
-                    data_loc.extend([x, y])
-                    x_.append(x)
-                    y_.append(y)
+        while time.time() - start_time < 10:  # 10-second limit for each letter
+            frame, results = capture_and_process_frame(cap, hands)
+            predicted_character = make_prediction(model, results, frame)
+            if predicted_character:
+                is_correct = update_and_display(frame, target_letter, predicted_character)
 
-            # Generate prediction from the model
-            prediction = model.predict([np.asarray(data_loc)])
-            predicted_character = labels_dict[int(prediction[0])]
+                # Update progress after each attempt
+                if is_correct:
+                    progress[target_letter]['correct'] += 1
+                progress[target_letter]['attempts'] += 1
+                save_progress(progress, file_path)
 
-            # Display the prediction on the frame
-            cv2.putText(frame, predicted_character, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 3)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        # Display the target letter on the frame
-        cv2.putText(frame, f"Show letter: {target_letter}", (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow("frame", frame)
-
-        elapsed_time = time.time() - start_time
-        if elapsed_time > 10:  # 10-second time limit
-            break
-
-        key = cv2.waitKey(1)
-        if key == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-    # Determine if the prediction was correct
-    is_correct = predicted_character.lower() == target_letter.lower()
-    return is_correct
-
-
-
-def play_game(model):
-    pass
-
 def main():
-    pass
+    SCRIPT_DIR, DATA_DIR = get_directory()
+    model = open_model(SCRIPT_DIR, DATA_DIR)
+    progress_file = "user_progress.pkl"
+    user_progress = load_progress(progress_file)
+    practice_loop(model, user_progress, progress_file)
 
 if __name__ == "__main__":
     main()
